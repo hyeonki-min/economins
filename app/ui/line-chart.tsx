1f970fea3ab70597ad1cda90fd9e9b5c9f66d3ba
ho9science <full17@naver.com>
2024-01-06 01:13:31 +0900

    feat: add chartjs-adapter-date-fns

--- app/ui/line-chart.tsx
+++ app/ui/line-chart.tsx
@@ -6,19 +6,21 @@ import {
   Tooltip,
   PointElement,
   LineElement,
+  TimeScale
 } from "chart.js";
 import type { ChartData, ChartOptions } from 'chart.js';
 import { Line } from "react-chartjs-2";
 import { getChartData, Rate } from '@/app/lib/json';
 import { useEffect, useState } from "react";
-
+import "chartjs-adapter-date-fns";
 // Register ChartJS components using ChartJS.register
 ChartJS.register(
   CategoryScale,
   LinearScale,
   PointElement,
   LineElement,
-  Tooltip
+  Tooltip,
+  TimeScale
 );
 
 ChartJS.register(CategoryScale, /* ... */)
@@ -34,17 +36,24 @@ export default function MyLineChart({ dataType }: { dataType: Candidate[] }) {
   });
 
   const [chartOptions, setChartOptions] = useState<ChartOptions<'line'>>({});
-  const backgroundColors = ['red', 'blue', 'orange', 'green', 'yellow', 'purple', 'gray'];
+  
   useEffect(() => {
+    const backgroundColors = ['red', 'blue', 'orange', 'green', 'yellow', 'purple', 'gray'];
     var datasets: any = [];
+    const axisPositionSet = new Set<string>();
 
     getChartData().then((data) => {
+      let firstAxis = '';
       dataType.forEach((element, index) => {
+        axisPositionSet.add(element.type);
+        if (axisPositionSet.size <= 1) {
+          firstAxis = element.type;
+        }
         datasets.push({
           'label': element.name,
           'data': data[element.name as keyof Rate],
           'backgroundColor': backgroundColors[index],
-          'yAxisID': element.type === 'rate'? 'y': 'y2'
+          'yAxisID': firstAxis === element.type ? 'y': 'y2',
         });
       });
       const getCheckedChartData: ChartData <'line'> = {
@@ -53,27 +62,37 @@ export default function MyLineChart({ dataType }: { dataType: Candidate[] }) {
       setChartData(
           getCheckedChartData
       );
-    });
-
-    setChartOptions({
-      responsive: true,
-      maintainAspectRatio: true,
-      scales: {
-        y2: {
-          type: 'linear',
-          display: true,
-          position: 'right',
-          grid: {
-            drawOnChartArea: false,
-          },
+      setChartOptions({
+        responsive: true,
+        maintainAspectRatio: true,
+        interaction: {
+          intersect: false,
+          mode: 'index',
         },
-        y: {
-          type: 'linear',
-          display: true,
-          position: 'left',
-          beginAtZero: true
-        }
-      }
+        scales: {
+          y2: {
+            type: 'linear',
+            position: 'right',
+            grid: {
+              drawOnChartArea: false,
+            },
+          },
+          y: {
+            type: 'linear',
+            position: 'left',
+            beginAtZero: true
+          },
+          x: {
+            type: 'time',
+            time: {
+              displayFormats: {
+                year: 'yyyy-MM'
+              },
+              tooltipFormat: 'yyyy-MM' 
+            }
+          }
+        },
+      });  
     });
   }, [dataType]);
 
