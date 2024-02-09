0ac693eff2b52fb61c434f1fd3153062c5328269
ho9science <full17@naver.com>
2024-02-10 00:29:56 +0900

    build: set of adding revalidate

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,16 @@ import createPresignedUrl from '@/app/lib/economins';
 import EconominsLogo from '@/app/ui/logo';
 import Category from '@/app/ui/category';
 import { Indicator } from '@/app/lib/definitions';
+import { notFound } from 'next/navigation';
 
+export const revalidate = 1
 
 export default async function Page() {
-  const allElement: Indicator[] = await createPresignedUrl({ key: 'main/main' });
-
+  
+  const allElement = await createPresignedUrl({ key: 'main/main' });
+  if (allElement.length < 1) {
+    notFound();
+  }
   return (
     <main className="flex-col bg-slate-50">
       <div className="flex sticky top-0 h-12 shrink-0 items-end p-2 md:h-12">
