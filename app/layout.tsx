b4819157bed6e3086c66782c819d8bd00bdc741b
ho9science <full17@naver.com>
2025-07-12 23:51:25 +0900

    chore: add og description

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,10 @@ import { GoogleTagManager } from '@next/third-parties/google'
 export const metadata: Metadata = {
   title: {
     template: '%s | economins',
-    default: '경제 데이터 시각화 - economins',
+    default: '경제 데이터 시각화 비교 플랫폼 - economins',
+  },
+  openGraph: {
+    description: '대한민국 기준금리 vs 부동산 실거래가 등 거시경제 지표를 시각적으로 비교하는 서비스'
   },
   description: '대한민국 기준금리 vs 부동산 실거래가 등 거시경제 지표를 시각적으로 비교하는 서비스',
   metadataBase: new URL('https://economins.com'),
