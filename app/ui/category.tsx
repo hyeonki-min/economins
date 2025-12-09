7fdee2a9bf7bc29cb78eb85e7ea694a1686a6114
hyeonki Min <full17@naver.com>
2025-12-09 14:22:33 +0900

    fix: update category color

--- app/ui/category.tsx
+++ app/ui/category.tsx
@@ -31,7 +31,7 @@ export default function Category({ elements }: { elements: Indicator[] }) {
               peer-checked:bg-neutral-900 peer-checked:text-white
             `,
               {
-                'bg-neutral-900 text-white': type === types,
+                'bg-slate-900 text-slate-100': type === types,
               },
             )}
           >
