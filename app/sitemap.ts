1e6c5c7e6a920e3475ced203434624ae0cabbb18
hyeonki Min <full17@naver.com>
2026-01-06 01:00:53 +0900

    feat: add sitemap.xml and robots.txt

---
+++ app/sitemap.ts
@@ -0,0 +1,36 @@
+import { MetadataRoute } from "next";
+import { VALID_POLICY_IDS } from "@/app/lib/policy";
+import { fetchDataset } from '@/app/lib/fetch-data';
+import { Indicator } from '@/app/lib/definitions';
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const baseUrl = "https://economins.com";
+
+  const allElement = await fetchDataset<Indicator>("main/main");
+  const seriesIds = allElement.map((item) => item.id);
+
+  const urls: MetadataRoute.Sitemap = [];
+
+  urls.push({
+    url: baseUrl,
+    priority: 1.0,
+  });
+
+  seriesIds.forEach((id) => {
+    urls.push({
+      url: `${baseUrl}/series/${id}`,
+      priority: 0.8,
+      changeFrequency: "weekly",
+    });
+  });
+
+  VALID_POLICY_IDS.forEach((date) => {
+    urls.push({
+      url: `${baseUrl}/monetary-policy/${date}`,
+      priority: 0.7,
+      changeFrequency: "monthly",
+    });
+  });
+
+  return urls;
+}
